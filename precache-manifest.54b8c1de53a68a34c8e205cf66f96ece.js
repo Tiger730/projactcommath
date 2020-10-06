abb882e02cb35cb38e85835dee989cb745dfcd6c
Tiger730 <jkkrit.mo.61@ubu.ac.th>
2020-10-07 05:42:20 +0700

    Updates

---
+++ precache-manifest.54b8c1de53a68a34c8e205cf66f96ece.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783f010d906848a234ad8b996552f23f",
+    "url": "/projectcommath/index.html"
+  },
+  {
+    "revision": "c4da7041bfeedb29a011",
+    "url": "/projectcommath/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "96e5f62c7d6e38ad0afc",
+    "url": "/projectcommath/static/js/2.302baa6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projectcommath/static/js/2.302baa6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4da7041bfeedb29a011",
+    "url": "/projectcommath/static/js/main.4beaba2d.chunk.js"
+  },
+  {
+    "revision": "95cc8a1a79cc437eeed4",
+    "url": "/projectcommath/static/js/runtime-main.3ccd2308.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/projectcommath/static/media/logo.5d5d9eef.svg"
+  }
+]);
